5a22ea7425cfd645f164823e3bcf87830e43b256
pandorasNox <itspandorasnox@gmail.com>
2018-04-08 23:12:49 +0200

    adr for basic service setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
+
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -10,15 +11,15 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+// ... not in use here ...
 
+//app middleware setup
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
+//app middleware ROUTING setup
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
